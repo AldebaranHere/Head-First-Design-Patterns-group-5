c151352126fe27fd0a73b848a5e97d43f5f2ccc4
Daya Adianto <addianto@users.noreply.github.com>
2022-04-21 02:21:29 +0700

    Temporarily fix the codebase in prototype package

--- src/headfirst/designpatterns/prototype/Client.java
+++ src/headfirst/designpatterns/prototype/Client.java
@@ -2,17 +2,19 @@ package headfirst.designpatterns.prototype;
 
 public class Client {
 	public static void main(String[] args) {
-		Prototype p1 = new ConcretePrototype1();
-		Prototype p2 = new ConcretePrototype2();
-		
+		// TODO Update code example to match with the description in the 2nd ed. of the book
+		//      For now, the following edit was made to make the codebase compilable.
+		Monster p1 = new Dragon("Dragon", true);
+		Monster p2 = new Drakon("Drakon", 1, true);
+
 		// ... later ...
 		operation(p1);
 		operation(p2);
 	}
-	
-	public static Prototype operation(Prototype p) {
+
+	public static Monster operation(Monster p) {
 		// This code doesn't know or care what the concrete type of p is
-		Prototype pCopy = null;
+		Monster pCopy = null;
 		try {
 			pCopy = p.copy();
 			// do something useful with pCopy
